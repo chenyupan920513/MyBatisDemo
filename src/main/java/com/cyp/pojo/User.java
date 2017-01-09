9175bfa1a999e18c6b0470c976e4365689c73513
pierre <chenyupan920513@163.com>
2017-01-10 00:02:29 +0800

    first commit

---
+++ src/main/java/com/cyp/pojo/User.java
@@ -0,0 +1,53 @@
+package com.cyp.pojo;
+
+import java.util.Date;
+
+public class User {
+
+	public int id;
+	public String userName;
+	public Date birthday;
+	public String sex;
+	public String address;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
